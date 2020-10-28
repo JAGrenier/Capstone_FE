27cb7456d8a1b550cd36bc7d9162f11bf23b1eee
github username <github email address>
2020-10-28 09:53:01 -0600

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './routes/Home';
+import RestaurantDetailsPage from './routes/RestaurantDetailsPage';
+import UpdatePage from './routes/UpdatePage';
+
 
 const App = () => {
     return <div>
-        app
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route  path="/restaurants:id/update" component={UpdatePage} />
+                <Route  path="/restaurants:id" component={RestaurantDetailsPage} />
+            </Switch>    
+        </Router>
     </div>
-}
+};
 
 export default App;
