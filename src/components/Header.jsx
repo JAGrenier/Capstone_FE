86442c55a5e3fc99474171c27706b6cacd6fb91c
github username <github email address>
2020-10-28 11:42:13 -0600

    form and table created

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../Logo.png'
+
+const Header = () => {
+    return (
+        <div>
+            <img src={Logo} alt="logo" />
+        </div>
+    )
+}
+
+export default Header
