33c64cce6edee1636cfd1afc0f97a688b5795ecf
github username <github email address>
2020-10-28 13:25:10 -0600

    context API implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import RestaurantList from './components/RestaurantList';
+import { RestaurantsContext, RestaurantsContextProvider } from './Context/RestaurantsContext';
 import Home from './routes/Home';
 import RestaurantDetailsPage from './routes/RestaurantDetailsPage';
 import UpdatePage from './routes/UpdatePage';
 
 
 const App = () => {
-    return <div>
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route  path="/restaurants:id/update" component={UpdatePage} />
-                <Route  path="/restaurants:id" component={RestaurantDetailsPage} />
-            </Switch>    
-        </Router>
-    </div>
-};
+    return( 
+        <RestaurantsContextProvider>
+            <div className="container">
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route  path="/restaurants:id/update" component={UpdatePage} />
+                        <Route  path="/restaurants:id" component={RestaurantDetailsPage} />
+                    </Switch>    
+                </Router>
+            </div>
+        </RestaurantsContextProvider>
+)};
 
 export default App;
