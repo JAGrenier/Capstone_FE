94cc79520478a82686c935d0da457f6863524ca4
github username <github email address>
2020-10-29 13:17:40 -0600

    add review component and review cards

--- src/routes/RestaurantDetailsPage.jsx
+++ src/routes/RestaurantDetailsPage.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import RestaurantFinder from '../apis/RestaurantFinder'
+import AddReview from '../components/AddReview'
+import Reviews from '../components/Reviews'
 import StarRating from '../components/StarRating'
 import { RestaurantsContext } from '../Context/RestaurantsContext'
 
@@ -22,7 +24,14 @@ const RestaurantDetailsPage = (props) => {
     },[])
     return(
         <div>
-            {selectedRestaurant && <StarRating rating={4}/>}
+            {selectedRestaurant && (
+                <>
+                <div className="mt-3">
+                    <Reviews />
+                    <AddReview />
+                </div>
+                </>
+            )}
         </div>
 
     ) 
