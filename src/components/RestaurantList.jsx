bb9614f9e118c16ac9a56c77fe8ae988af44e602
github username <github email address>
2020-11-02 11:24:50 -0700

    cards rendering with iamges and rating

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -2,7 +2,22 @@ import React, { useEffect, useContext } from 'react'
 import { useHistory } from 'react-router-dom'
 import RestaurantFinder from '../apis/RestaurantFinder'
 import { RestaurantsContext } from '../Context/RestaurantsContext'
-import StarRating from './StarRating'
+import StarRating from './StarRating';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 const RestaurantList = (props) => {
     const {restaurants, setRestaurants } = useContext(RestaurantsContext)
@@ -52,34 +67,75 @@ const RestaurantList = (props) => {
           ) 
     }
 
+    const useStyles = makeStyles((theme) => ({
+        root: {
+          maxWidth: 345
+        },
+        media: {
+          height: 0,
+          paddingTop: "56.25%" // 16:9
+        },
+        expand: {
+          transform: "rotate(0deg)",
+          marginLeft: "auto",
+          transition: theme.transitions.create("transform", {
+            duration: theme.transitions.duration.shortest
+          })
+        },
+        expandOpen: {
+          transform: "rotate(180deg)"
+        },
+        avatar: {
+          backgroundColor: red[500]
+        }
+      }));
+        const classes = useStyles();
+        const [expanded, setExpanded] = React.useState(false);
+
+        const handleExpandClick = () => {
+            setExpanded(!expanded);
+    };
+
     return (
         <div className="list-group">
-            <table className="table table-hover table-dark">
-                <thead> 
-                    <tr className="bg-primary">
-                        <th scope="col">Restaurant</th>
-                        <th scope="col">Location</th>
-                        <th scope="col">Price Range</th>
-                        <th scope="col">Ratings</th>
-                        <th scope="col">Add a Review</th>
-                        <th scope="col">See All Reviews</th>
-                    </tr>
-                </thead>
-                <tbody> 
                     {restaurants && restaurants.map((restaurant) => {
                         return(
-                            <tr>
-                                <td>{restaurant.name}</td>
-                                <td>{restaurant.location}</td>
-                                <td>{"$".repeat(restaurant.price_range)}</td>
-                                <td>{renderRating(restaurant)}</td>
-                                <td><button onClick={() => handleRestaurantSelect(restaurant.id)} key={restaurant.id} className="btn btn-warning">Add a Review</button></td>
-                                <td><button onClick={(event) => handleDelete(event, restaurant.id)} className="btn btn-warning">View Reviews</button></td>
-                            </tr>
+                            <>
+                                <Card className={classes.root} >
+                                    <CardHeader
+                                        action={
+                                            <IconButton aria-label="settings">
+                                                <MoreVertIcon />
+                                            </IconButton>
+                                        } 
+                                        title={restaurant.name}
+                                        />
+                                            <CardMedia
+                                            className={classes.media}
+                                            image="https://images.unsplash.com/photo-1571407970349-bc81e7e96d47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1525&q=80jpg"
+                                            title="Paella dish"
+                                            />
+                                                <CardContent>
+                                                    <Typography variant="body2" color="textSecondary" component="p">
+                                                    <span>{renderRating(restaurant)}</span>
+                                                    </Typography>
+                                                </CardContent>
+                                    <Button 
+                                        variant="contained"
+                                        color="secondary"  
+                                        onClick={() => handleRestaurantSelect(restaurant.id)} 
+                                        key={restaurant.id}>
+                                    Add a Review
+                                    </Button>
+                                    <Button 
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={(event) => handleDelete(event, restaurant.id)} >
+                                    View Reviews</Button>
+                                </Card>
+                            </>
                         )
                     })}
-                </tbody>
-            </table>
             
         </div>
     )
